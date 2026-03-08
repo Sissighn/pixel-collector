f15ad9dd406ebde1b0ffa8ca91cb09f8b34df990
Sissighn <99798403+Sissighn@users.noreply.github.com>
2026-03-08 02:50:20 +0100

    add Player class with input handling, updating, and rendering methods

---
+++ src/Player.cpp
@@ -0,0 +1,34 @@
+#include "Player.h"
+
+Player::Player()
+{
+    shape.setRadius(30.f);
+    shape.setFillColor(sf::Color::Green);
+    shape.setPosition({400,300});
+
+    speed = 0.3f;
+}
+
+void Player::handleInput()
+{
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
+        shape.move({0, -speed});
+
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
+        shape.move({0, speed});
+
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
+        shape.move({-speed, 0});
+
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
+        shape.move({speed, 0});
+}
+
+void Player::update()
+{
+}
+
+void Player::draw(sf::RenderWindow& window)
+{
+    window.draw(shape);
+}
