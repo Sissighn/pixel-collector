272b59e366557f38c264bf8d33f8c832f1fa12d6
Sissighn <99798403+Sissighn@users.noreply.github.com>
2026-03-08 03:47:15 +0100

    refactor Coin class to inherit from Entity and override update and draw methods

--- src/Coin.h
+++ src/Coin.h
@@ -1,12 +1,14 @@
 #pragma once
 #include <SFML/Graphics.hpp>
-
-class Coin
+#include "Entity.h" 
+class Coin : public Entity
 {
 public:
     Coin(float x, float y);
 
-    void draw(sf::RenderWindow& window);
+    void update(float deltaTime) override;
+    void draw(sf::RenderWindow& window) override;
+
     sf::Vector2f getPosition() const;
     float getRadius() const;
 
