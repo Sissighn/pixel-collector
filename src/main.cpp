5f43407b32c93c407fbf95068f13f77c412d6288
Sissighn <99798403+Sissighn@users.noreply.github.com>
2026-03-08 02:50:26 +0100

    refactor main function to utilize Game class for running the application

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,7 @@
-#include <SFML/Graphics.hpp>
+#include "Game.h"
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({800, 600}), "Tiny Collector");
-
-    while (window.isOpen())
-    {
-        while (auto event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-                window.close();
-        }
-
-        window.clear(sf::Color::Black);
-        window.display();
-    }
+    Game game;
+    game.run();
 }
