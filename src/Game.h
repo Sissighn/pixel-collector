2222180f41392afd4e10211778a5d65b8874b84f
Sissighn <99798403+Sissighn@users.noreply.github.com>
2026-03-08 03:23:55 +0100

    add random number generation for coin placement in Game class

--- src/Game.h
+++ src/Game.h
@@ -3,6 +3,7 @@
 #include "Player.h"
 #include "Coin.h"
 #include <vector>
+#include <random>
 
 class Game
 {
@@ -20,4 +21,14 @@ private:
 
     std::vector<Coin> coins;
     int score;
+
+    sf::Font font;
+    sf::Text scoreText{font};
+
+    std::mt19937 rng;
+    std::uniform_real_distribution<float> xDist;
+    std::uniform_real_distribution<float> yDist;
+
+    sf::Clock clock;
+    float deltaTime;
 };
