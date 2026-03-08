f8a188b899bacca7d982fd383bbac912c2b91f3e
Sissighn <99798403+Sissighn@users.noreply.github.com>
2026-03-08 02:50:13 +0100

    add Player class with methods for input handling, updating, and rendering

---
+++ src/Player.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+class Player
+{
+public:
+    Player();
+
+    void handleInput();
+    void update();
+    void draw(sf::RenderWindow & window);
+
+private:
+    sf::CircleShape shape;
+    float speed;
+};
